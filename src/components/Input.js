2ab1e8613c3329d6e1b5adb1ae3514d8012138ec
RafaelHDSV <rafaebolt@gmail.com>
2024-06-16 20:08:31 -0300

    feat: adding weather from input compontent

---
+++ src/components/Input.js
@@ -0,0 +1,7 @@
+export default function Input({ inputValue }) {
+    return (
+        <div className="input-container">
+            {/* <span>{inputValue[0].location.name}</span> */}
+        </div>
+    )
+}
